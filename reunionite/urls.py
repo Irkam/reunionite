791669c5624a1f9b9b3e0533a95c05d53fafd56d
Mariana Andujar <mariana.andujarf@gmail.com>
2015-01-18 23:08:17 +0100

    create design try logout but fail

--- reunionite/urls.py
+++ reunionite/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/resgistration/logout.html'}),
     url(r'^register/$', register.RegisterView.as_view(), name="register"),
     url(r'^user/$', user.UserView.as_view(), name="user"),
     url(r'^user/(?P<uid>\d+)$', user.UserView.as_view(), name="user"),
